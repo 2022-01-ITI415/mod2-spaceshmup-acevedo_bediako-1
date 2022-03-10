e727a456b13c6bc4e9347d264db2838f19a53ec0
RyanAnthony1204 <racevedo1204@gmail.com>
2022-03-10 14:38:28 -0500

    Updated Enemy 5 Script
    Updated the Enemy 5 Script to spawn the boss' minions correctly.

--- Assets/__Scripts/Enemy_5.cs
+++ Assets/__Scripts/Enemy_5.cs
@@ -3,6 +3,81 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy_5 : Enemy_4 {
-
     
+    [Header("Set in Inspector")]
+    public GameObject[] prefabEnemies; // Array of Enemy prefabs
+    public float enemySpawnPerSecond = 0.01f; // # Enemies/second
+    public float enemyPadding = 2.0f;
+    private Vector3 p0, p1;
+    private float timeStart;
+    private float duration = 4;
+
+    private void Start()
+    {
+        p0 = p1 = pos;
+
+        InitMovement();
+
+        // Cache GameObject & Material of each Part in parts
+        Transform t;
+        foreach (Part prt in parts)
+        {
+            t = transform.Find(prt.name);
+            if (t != null)
+            {
+                prt.go = t.gameObject;
+                prt.mat = prt.go.GetComponent<Renderer>().material;
+            }
+        }
+
+
+        Invoke("SpawnEnemy", 1f / enemySpawnPerSecond);
+    }
+
+    public void SpawnEnemy()
+    {
+        // Pick a random Enemy prefab to instantiate
+        int ndx = Random.Range(0, prefabEnemies.Length);
+        GameObject go = Instantiate<GameObject>(prefabEnemies[ndx]);
+
+
+        // Set the initial position for the spawned Enemy
+        Vector3 pos = Vector3.zero;
+        float xMin = transform.position.x + enemyPadding;
+        float xMax = transform.position.x - enemyPadding;
+        pos.x = Random.Range(xMin, xMax);
+        pos.y = transform.position.y + enemyPadding;
+        go.transform.position = pos;
+
+        // Invoke SpawnEnemy() again
+        Invoke("SpawnEnemy", 1f / enemySpawnPerSecond);
+    }
+
+     void InitMovement()
+    {
+        p0 = p1; // Set p0 to the old p1
+        // Assign a new on-screen location to p1
+        float widMinRad = bndCheck.camWidth - bndCheck.radius;
+        float hgtMinRad = bndCheck.camHeight - bndCheck.radius;
+        p1.x = Random.Range(-widMinRad, widMinRad);
+        p1.y = Random.Range(-hgtMinRad, hgtMinRad);
+
+        // Reset the time
+        timeStart = Time.time;
+    }
+
+    public override void Move()
+    {
+        // This completely overrides Enemy.Move() with a linear interpolation
+        float u = (Time.time - timeStart) / duration;
+
+        if (u >= 1)
+        {
+            InitMovement();
+            u = 0;
+        }
+
+        u = 1 - Mathf.Pow(1 - u, 2); // Apply Ease Out easing to u
+        pos = ((1 - u) * p0) + (u * p1);// Simple linear interpolation
+    }
 }
